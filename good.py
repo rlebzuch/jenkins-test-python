de0cc7cf6a76965e257e9bfaf95e59ea69af059c
kret <rlebzuch@gmail.com>
2026-06-27 19:47:25 +0200

    Pierwszy commit: 2 pliki Python + Jenkinsfile

---
+++ good.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print(f"2 + 3 = {add(2, 3)}")
+    print(f"4 * 5 = {multiply(4, 5)}")
